2df1116804a280d3c9b3f1af58fd140737dcc402
guferreira1 <gutoconsultor.imoveis@hotmail.com>
2022-11-21 10:56:13 -0300

    feat: add Products Cart component

---
+++ components/card-items/CardComponent.tsx
@@ -0,0 +1,83 @@
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { FiShoppingBag } from "react-icons/fi";
+
+import { CardContainer } from "./CardStyles";
+
+interface iProducts {
+  id: number;
+  name: string;
+  brand: string;
+  description: string;
+  photo: string;
+  price: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+const CardComponent = () => {
+  const [isProduct, setIsProduct] = useState<iProducts[]>(
+    []
+  );
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    try {
+      const res = await fetch(
+        "https://mks-frontend-challenge-api.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=DESC"
+      );
+
+      const data = await res.json();
+
+      setIsProduct(data.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <CardContainer>
+        <ul>
+          {isProduct.map((product) => (
+            <>
+              <li key={product.id}>
+                {" "}
+                <Image
+                  src={product.photo}
+                  alt={product.name}
+                  width="127"
+                  height="158"
+                />
+                <div className="itemInfo">
+                  <div className="itemName">
+                    <h2>{product.name}</h2>
+                  </div>
+                  <div className="itemPrice">
+                    <span>
+                      R${" "}
+                      {Number(product.price)
+                        .toFixed(2)
+                        .toString()
+                        .replace(".", ",")}
+                    </span>
+                  </div>
+                </div>
+                <p>{product.description}</p>
+                <button>
+                  <FiShoppingBag />
+                  COMPRAR
+                </button>
+              </li>
+            </>
+          ))}
+        </ul>
+      </CardContainer>
+    </>
+  );
+};
+
+export default CardComponent;
--- components/layout/LeyoutStyles.ts
+++ components/layout/LeyoutStyles.ts
@@ -1,7 +1,16 @@
 import styled from "styled-components";
 import Colors from "../../themes/themeColors";
 
-export const MainContainer = styled.main`
-  min-height: 90vh;
-  background-color: ${Colors.gray2};
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  width: 100%;
+  min-height: 100vh;
+`;
+
+export const MainContainer = styled.div`
+  /* min-height: 90vh; */
+  /* background-color: ${Colors.gray2}; */
 `;
